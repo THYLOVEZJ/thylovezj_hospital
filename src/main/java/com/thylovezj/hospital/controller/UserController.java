86c0c19bb6043c5cf420af6f0df80df7e73af5aa
thylovezj <1105864486@qq.com>
2022-04-20 16:32:36 +0800

    thy

--- src/main/java/com/thylovezj/hospital/controller/UserController.java
+++ src/main/java/com/thylovezj/hospital/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.thylovezj.hospital.controller;
 
+import com.thylovezj.hospital.common.ApiRestResponse;
+import com.thylovezj.hospital.exception.ThylovezjHospitalException;
+import com.thylovezj.hospital.exception.ThylovezjHospitalExceptionEnum;
 import com.thylovezj.hospital.model.pojo.User;
+import com.thylovezj.hospital.model.request.AddUserReq;
 import com.thylovezj.hospital.service.UserService;
 import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +19,21 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/test")
-    @ApiOperation("第一个Api接口")
-    public User test(Integer userId){
-        return userService.getUser();
+
+    @PostMapping("/rigister")
+    @ApiOperation("添加用户")
+    public ApiRestResponse register(@RequestBody AddUserReq addUserReq) {
+        if (StringUtils.isEmpty(addUserReq.getUsername())) {
+            return ApiRestResponse.error(ThylovezjHospitalExceptionEnum.NEED_USER_NAME);
+        }
+        if (StringUtils.isEmpty(addUserReq.getPassword())) {
+            return ApiRestResponse.error(ThylovezjHospitalExceptionEnum.NEED_PASSWORD);
+        }
+        int role = addUserReq.getRole();
+        if (role != 0 && role != 1 && role != 2) {
+            return ApiRestResponse.error(ThylovezjHospitalExceptionEnum.ROLE_ERROR);
+        }
+        userService.Register(addUserReq.getUsername(), addUserReq.getPassword(), role);
+        return ApiRestResponse.success();
     }
 }
--- src/main/java/com/thylovezj/hospital/model/pojo/Problem.java
+++ src/main/java/com/thylovezj/hospital/model/pojo/Problem.java
@@ -19,6 +19,8 @@ public class Problem {
 
     private String check;
 
+    private String score;
+
     public Long getId() {
         return id;
     }
@@ -81,5 +83,13 @@ public class Problem {
 
     public void setCheck(String check) {
         this.check = check == null ? null : check.trim();
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score == null ? null : score.trim();
     }
 }
